35c54a1b3f0fae1aae32752f38e04c35116a06d9
Remi-Andre Olsen <remi-andre.olsen@scilifelab.se>
2014-11-20 17:33:44 +0100

    First pass whitespace and 80-ish columns fix

--- utils/OpGen_util.py
+++ utils/OpGen_util.py
@@ -24,7 +24,6 @@ reverse_complements = {
     "N": "N"
 }
 
-
 def main(args):
     workingDir     = os.getcwd()
     assembly       = args.assembly
@@ -45,16 +44,16 @@ def main(args):
 def produce_consensus(opgen_report, contigsLengthDict, contigsSequencesDict, output):
 
     # starting to parse opgen report, since the csv file is unstructured I need to parse it
-    placement        = "{}.placement".format(opgen_report)
-    stats            = "{}.stats".format(opgen_report)
-    gaps_overlaps    = "{}.gaps".format(opgen_report)
+    placement = "{}.placement".format(opgen_report)
+    stats = "{}.stats".format(opgen_report)
+    gaps_overlaps = "{}.gaps".format(opgen_report)
     unplaced_contigs = "{}.unplaced".format(opgen_report)
     with open(opgen_report, 'rb') as unstructured_file:
-        placement_file          = open(placement, 'w')
-        stats_file              = open(stats, 'w')
-        gaps_overlaps_file      = open(gaps_overlaps, 'w')
-        unplaced_contigs_file   = open(unplaced_contigs, 'w')
-        current_writing_file    = placement_file
+        placement_file = open(placement, 'w')
+        stats_file = open(stats, 'w')
+        gaps_overlaps_file = open(gaps_overlaps, 'w')
+        unplaced_contigs_file = open(unplaced_contigs, 'w')
+        current_writing_file = placement_file
         for row in unstructured_file:
             if "N50" in row:
                 current_writing_file = stats_file
@@ -68,21 +67,21 @@ def produce_consensus(opgen_report, contigsLengthDict, contigsSequencesDict, out
         stats_file.close()
         gaps_overlaps_file.close()
 
-   # I need a list with one element for each base in my map. THe problem is that
-   # the OpGen report does not tell the length of the maps, therefore I need to 
-   # find the maximum limit between the placments part and the gapped part
-    Maps            = {}
-    Maps_length     = {}
-    ContigsToMaps   = {}
-    MapsToContigs   = {}
+    # I need a list with one element for each base in my map. THe problem is that
+    # the OpGen report does not tell the length of the maps, therefore I need to 
+    # find the maximum limit between the placments part and the gapped part
+    Maps = {}
+    Maps_length = {}
+    ContigsToMaps = {}
+    MapsToContigs = {}
     with open(placement, 'rb') as csvfile:
         opgenCSV = csv.reader(csvfile, delimiter='\t')
         #Chromosome, Start, End, Contig, Start, End, Orientation
         header = opgenCSV.next()
         for row in opgenCSV:
             if len(row) > 0:
-                #print ', '.join(row)
-                (Map, Map_Start, Map_End, Contig, Ctg_Start, Ctg_End, Orientation) = (row[0], int(row[1]), int(row[2]), row[3], int(row[4]), int(row[5]), row[6])
+                (Map, Map_Start, Map_End, Contig, Ctg_Start, Ctg_End, Orientation) = \
+                        (row[0], int(row[1]), int(row[2]), row[3], int(row[4]), int(row[5]), row[6])
                 Contig = Contig.split(" ")[0]
                 if not ContigsToMaps.has_key(Contig):
                     ContigsToMaps[Contig] = [[Map, Map_Start, Map_End, Ctg_Start, Ctg_End, Orientation]]
@@ -94,7 +93,8 @@ def produce_consensus(opgen_report, contigsLengthDict, contigsSequencesDict, out
                 else:
                     if Map_End > Maps_length[Map]:
                         Maps_length[Map] = Map_End
-                        MapsToContigs[Map].append([Map_Start, Map_End, Contig, Ctg_Start, Ctg_End, Orientation])
+                        MapsToContigs[Map].append([Map_Start, Map_End, Contig, 
+                            Ctg_Start, Ctg_End, Orientation])
                     else:
                         print "{} {}".format(Map_End, Maps_length[Map])
 
@@ -105,7 +105,8 @@ def produce_consensus(opgen_report, contigsLengthDict, contigsSequencesDict, out
         header = opgenCSV.next()
         for row in opgenCSV:
             if len(row) > 0:
-                (Map, Type, Map_Start, Map_End, Length) = (row[0], row[1], int(row[2]), int(row[3]), int(row[4]))
+                (Map, Type, Map_Start, Map_End, Length) = \
+                        (row[0], row[1], int(row[2]), int(row[3]), int(row[4]))
                 if not Maps_length.has_key(Map):
                     Maps_length[Map] = Map_End
                 else:
@@ -114,8 +115,8 @@ def produce_consensus(opgen_report, contigsLengthDict, contigsSequencesDict, out
 
     # now Maps_length contains the lenght of the Maps... thank you so much OpGen!!!!!
     multipleHittedPositions = 0
-    rescuedBases            = 0
-    conflictingBases        = 0
+    rescuedBases = 0
+    conflictingBases = 0
 
     for Map in Maps_length:
         Maps[Map] = ["n"] * Maps_length[Map]
@@ -123,13 +124,13 @@ def produce_consensus(opgen_report, contigsLengthDict, contigsSequencesDict, out
         if MapsToContigs[Map] is not None: # if this map has some contigs aligning
             for hit in MapsToContigs[Map]:
                 start_on_Map = hit[0] - 1
-                end_on_Map   = hit[1] - 1
-                Ctg          = hit[2]
+                end_on_Map = hit[1] - 1
+                Ctg = hit[2]
                 start_on_Ctg = hit[3] - 1
-                end_on_Ctg   = hit[4] - 1
-                orientation  = hit[5]
+                end_on_Ctg = hit[4] - 1
+                orientation = hit[5]
                 #extract the seuqence
-                sequence     = contigsSequencesDict[Ctg][start_on_Ctg:end_on_Ctg]
+                sequence = contigsSequencesDict[Ctg][start_on_Ctg:end_on_Ctg]
                 if orientation == '-1':
                     sequence = revcom(sequence)
                 letters = list(sequence)
@@ -148,7 +149,7 @@ def produce_consensus(opgen_report, contigsLengthDict, contigsSequencesDict, out
                     else:
                         Maps[Map][index] = base
                     index += 1
-                #print hit
+    #print hit
     print "position in th emap covered more than once {}".format(multipleHittedPositions)
     print "rescued bases {}".format(rescuedBases)
     print "conflicting bases {}".format(conflictingBases)
@@ -181,16 +182,16 @@ def find_problems_in_maps(opgen_report, contigsLengthDict, contigsSequencesDict)
     ###TODO: this function is still to be defined.
     print opgen_report
     # startint to parse opgen report, since the csv file is unstructured I need to parse it
-    placement        = "{}.placement".format(opgen_report)
-    stats            = "{}.stats".format(opgen_report)
-    gaps_overlaps    = "{}.gaps".format(opgen_report)
+    placement = "{}.placement".format(opgen_report)
+    stats = "{}.stats".format(opgen_report)
+    gaps_overlaps = "{}.gaps".format(opgen_report)
     unplaced_contigs = "{}.unplaced".format(opgen_report)
     with open(opgen_report, 'rb') as unstructured_file:
-        placement_file          = open(placement, 'w')
-        stats_file              = open(stats, 'w')
-        gaps_overlaps_file      = open(gaps_overlaps, 'w')
-        unplaced_contigs_file   = open(unplaced_contigs, 'w')
-        current_writing_file    = placement_file
+        placement_file = open(placement, 'w')
+        stats_file = open(stats, 'w')
+        gaps_overlaps_file = open(gaps_overlaps, 'w')
+        unplaced_contigs_file = open(unplaced_contigs, 'w')
+        current_writing_file = placement_file
         for row in unstructured_file:
             if "N50" in row:
                 current_writing_file = stats_file
@@ -204,10 +205,12 @@ def find_problems_in_maps(opgen_report, contigsLengthDict, contigsSequencesDict)
         stats_file.close()
         gaps_overlaps_file.close()
 
-    # I need a list with one element for each base in my map. THe problem is that the OpGen report does not tell the length of the maps, therefore I need to find the maximum limit between the placments part and the gapped part
-    Maps            = {}
-    Maps_length     = {}
-    ContigsToMaps   = {}
+    # I need a list with one element for each base in my map. 
+    # The problem is that the OpGen report does not tell the length of the maps, 
+    # therefore I need to find the maximum limit between the placments part and the gapped part
+    Maps = {}
+    Maps_length = {}
+    ContigsToMaps = {}
     with open(placement, 'rb') as csvfile:
         opgenCSV = csv.reader(csvfile, delimiter='\t')
         #Chromosome, Start, End, Contig, Start, End, Orientation
@@ -215,7 +218,8 @@ def find_problems_in_maps(opgen_report, contigsLengthDict, contigsSequencesDict)
         for row in opgenCSV:
             if len(row) > 0:
                 #print ', '.join(row)
-                (Map, Map_Start, Map_End, Contig, Ctg_Start, Ctg_End, Orientation) = (row[0], int(row[1]), int(row[2]), row[3], int(row[4]), int(row[5]), row[6])
+                (Map, Map_Start, Map_End, Contig, Ctg_Start, Ctg_End, Orientation) = \
+                        (row[0], int(row[1]), int(row[2]), row[3], int(row[4]), int(row[5]), row[6])
                 Contig = Contig.split(" ")[0]
                 if not ContigsToMaps.has_key(Contig):
                     ContigsToMaps[Contig] = [[Map, Map_Start, Map_End, Ctg_Start, Ctg_End, Orientation]]
@@ -236,7 +240,8 @@ def find_problems_in_maps(opgen_report, contigsLengthDict, contigsSequencesDict)
         header = opgenCSV.next()
         for row in opgenCSV:
             if len(row) > 0:
-                (Map, Type, Map_Start, Map_End, Length) = (row[0], row[1], int(row[2]), int(row[3]), int(row[4]))
+                (Map, Type, Map_Start, Map_End, Length) = \
+                        (row[0], row[1], int(row[2]), int(row[3]), int(row[4]))
                 if not Maps_length.has_key(Map):
                     Maps_length[Map] = Map_End
                 else:
@@ -246,7 +251,8 @@ def find_problems_in_maps(opgen_report, contigsLengthDict, contigsSequencesDict)
 
     # print contigs that align in more than one map
     ##### now decide if these contigs are suspicious (need to be broken or they are ok)
-    ##### if a contig maps in two different places in the same way, then this is a real repeat. If there are two clear alignments then I must suggest to breack the contig
+    ##### if a contig maps in two different places in the same way, then this is
+    ##### a real repeat. If there are two clear alignments then I must suggest to breack the contig
     contigsInMultipleMaps = 0
     contigsWithErrors     = 0
     for Contig in ContigsToMaps:
@@ -256,9 +262,11 @@ def find_problems_in_maps(opgen_report, contigsLengthDict, contigsSequencesDict)
             contigsCoordinates     = []
             print Contig + " --> "
             for entry in ContigsToMaps[Contig]:
-                contigsCoordinates.append([entry[3],entry[4], (entry[3] - entry[3])/contigsLengthDict[Contig] ]) # save only start, end, and length of alignment
+                # save only start, end, and length of alignment
+                contigsCoordinates.append(
+                        [entry[3],entry[4], (entry[3] - entry[3])/contigsLengthDict[Contig] ])
                 print entry
-            ### check if this contig is an erroneus contig or a real repeat
+    ### check if this contig is an erroneus contig or a real repeat
     print "number of contigs aligning in multiple maps is {}".format(contigsInMultipleMaps)
 
     for Map in Maps_length:
@@ -267,40 +275,40 @@ def find_problems_in_maps(opgen_report, contigsLengthDict, contigsSequencesDict)
 
 def _compute_assembly_stats(assembly, genomeSize):
     stats_file_name = ".".join([assembly, "statistics", "txt"])
-    contigsLengthDict   = {}
-    contigsSequencesDict= {}
-    contigsLength       = []
-    totalLength         = 0
-    numContigs          = 0
-    maxContigLength     = 0
-    N50                 = 0
-    N80                 = 0
-    numNs               = 0
+    contigsLengthDict = {}
+    contigsSequencesDict = {}
+    contigsLength = []
+    totalLength = 0
+    numContigs = 0
+    maxContigLength = 0
+    N50 = 0
+    N80 = 0
+    numNs = 0
     
     with open(assembly, "r") as ref_fd:
-        fasta_header     = ref_fd.readline()
-        header           = (fasta_header.split(">")[1]).rstrip()
-        sequence         = ""
+        fasta_header = ref_fd.readline()
+        header = (fasta_header.split(">")[1]).rstrip()
+        sequence = ""
         for line in ref_fd:
             if line.startswith(">"):
                 contigsLength.append(len(sequence))
-                contigsLengthDict[header]    = len(sequence)
+                contigsLengthDict[header] = len(sequence)
                 contigsSequencesDict[header] = sequence
-                totalLength              += len(sequence)
-                numContigs               += 1
-                counter                   = Counter(sequence)
-                numNs                    += (counter['n'] + counter['N'])
-                sequence                  = ""
-                header                    = (line.split(">")[1]).rstrip()
+                totalLength += len(sequence)
+                numContigs += 1
+                counter = Counter(sequence)
+                numNs += (counter['n'] + counter['N'])
+                sequence = ""
+                header = (line.split(">")[1]).rstrip()
             else:
                 sequence+=line.rstrip()
         contigsLength.append(len(sequence))
         contigsLengthDict[header] = len(sequence)
         contigsSequencesDict[header] = sequence
-        totalLength              += len(sequence)
-        numContigs               += 1
-        counter                   = Counter(sequence)
-        numNs                    += (counter['n'] + counter['N'])
+        totalLength += len(sequence)
+        numContigs  += 1
+        counter = Counter(sequence)
+        numNs += (counter['n'] + counter['N'])
 
     if os.path.exists(stats_file_name):
         print "assembly stast file {} already created".format(stats_file_name)
@@ -346,7 +354,7 @@ def _build_new_reference(assembly, minCtgLength):
         print("The assembly name is not valid: {}".format(new_assembly_name))
         return new_assembly_name
 
-    new_assembly_name    = basename + ".{}.{}.{}".format(type, minCtgLength, extention)
+    new_assembly_name = basename + ".{}.{}.{}".format(type, minCtgLength, extention)
     new_assembly_name =  os.path.abspath(os.path.basename(new_assembly_name))
 
     if os.path.exists(new_assembly_name):
@@ -356,16 +364,16 @@ def _build_new_reference(assembly, minCtgLength):
     contigID = 1
     with open(new_assembly_name, "w") as new_ref_fd:
         with open(assembly, "r") as ref_fd:
-            fasta_header    = ref_fd.readline()
-            sequence        = ""
+            fasta_header = ref_fd.readline()
+            sequence = ""
             for line in ref_fd:
                 line = line
                 if line.startswith(">"):
                     if len(sequence) >= minCtgLength:
                         new_ref_fd.write(">contig{}\n".format(contigID))
                         new_ref_fd.write(sequence)
-                    sequence        = ""
-                    contigID        += 1
+                    sequence = ""
+                    contigID += 1
                 else:
                     sequence+=line
             if len(sequence) >= minCtgLength:
@@ -376,16 +384,22 @@ def _build_new_reference(assembly, minCtgLength):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--assembly',       help="assembly sequence", type=str)
-    parser.add_argument('--genome-size',    help="expected genome size", default=0, type=int)
-    parser.add_argument('--min-contig',     help="minimum contig length", default=2000, type=int)
-    parser.add_argument('--only-reference', action='store_true', default = False,  help="generates only the new refernce (i.e., only scaffolds longer than min-contig)")
-    parser.add_argument('--only-stats',     action='store_true', default = False,  help="generates only assembly stats")
-    parser.add_argument('--opgen-report',   help="op gen placment report", type=str)
-    parser.add_argument('--find-problems-in-maps', help="generates only the new refernce (i.e., only scaffolds longer than min-contig)", default=0, type=int)
-    parser.add_argument('--produce-consensus', help="generates only the new refernce (i.e., only scaffolds longer than min-contig)", default=0, type=int)
-
-    parser.add_argument('--output', help="aoutput header name", default="opgen_scaffolded_assembly", type=str)
+    parser.add_argument('--assembly', help="assembly sequence", type=str)
+    parser.add_argument('--genome-size', help="expected genome size", default=0, type=int)
+    parser.add_argument('--min-contig', help="minimum contig length", default=2000, type=int)
+    parser.add_argument('--only-reference', action='store_true', default = False,
+            help="generates only the new refernce (i.e., only scaffolds longer than min-contig)")
+    parser.add_argument('--only-stats', action='store_true', default = False,
+            help="generates only assembly stats")
+    parser.add_argument('--opgen-report',help="op gen placment report", type=str)
+    parser.add_argument('--find-problems-in-maps',
+            help="generates only the new refernce (i.e., only scaffolds longer than min-contig)",
+            default=0, type=int)
+    parser.add_argument('--produce-consensus',
+            help="generates only the new refernce (i.e., only scaffolds longer than min-contig)",
+            default=0, type=int)
+    parser.add_argument('--output', help="aoutput header name",
+            default="opgen_scaffolded_assembly", type=str)
     args = parser.parse_args()
     if args.genome_size == 0:
         print "genome size must be specified"
